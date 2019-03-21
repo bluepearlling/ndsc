e677d92150cce61ecde13be27c8473edf8777210
Mai <maisurahshuling@gmail.com>
2019-03-22 01:11:14 +0800

    test5

--- train.py
+++ train.py
@@ -81,7 +81,7 @@ def _train(X,y,save_model):
     }
 
     # training
-    text_clf_svm = Pipeline([('cv', cv), ('tfidf', tfidf),('svm', svm)])
+    text_clf_svm = Pipeline([('cv', cv), ('tfidf', tfidf), ('svm', svm)])
     gs_clf = GridSearchCV(text_clf_svm, parameters, n_jobs=-1, cv=skf.split(X, y), scoring='f1_micro')
 
     gs_clf = gs_clf.fit(X, y)
@@ -142,14 +142,14 @@ if __name__ == "__main__":
     save_model =False
     args = Utilities.process_arg("-save")
     out_dir = "/content/drive/My Drive/models"
-    csvs_folder_path = "/content/drive/My Drive/fashionfull.csv"
+    csvs_folder_path = "/content/drive/My Drive/gb.csv"
     category = "fashion"
     save = "true"
 
     if save == "t" or save == "T" or save == "true":
         save_model = True
 
-    train_csv_path = "/content/drive/My Drive/fashionfull.csv"
+    train_csv_path = "/content/drive/My Drive/gb.csv"
 
     csv_helper = CsvHelper()
     csv_helper.set_csv(train_csv_path) # set csv file as train_csv_path
