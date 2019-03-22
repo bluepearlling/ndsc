7eff50936053bf04c5ff9fdd147641cc0ccd558b
Mai <maisurahshuling@gmail.com>
2019-03-23 00:06:20 +0800

    test2

--- predict.py
+++ predict.py
@@ -86,7 +86,7 @@ def do_predict(label,category,csvs_folder_path):
 if __name__ == "__main__":
 
     args = Utilities.process_arg()
-    in_dir ="/content/drive/My Drive/models"
+    in_dir ="/content/drive/My Drive/models/"
     csvs_folder_path= "/content/drive/My Drive/csvs"
     category = "fashion"
 
